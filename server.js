1a76ff9cf90929c585b3996ef1961508698edb4f
Mikall <mitchelltlong@gmail.com>
2019-07-19 13:01:44 -0700

    Failed to lookup index

--- server.js
+++ server.js
@@ -15,8 +15,8 @@ var clientfunc = function() {
     });
 }
 
-app.get('/', function(req, res) {
-    res.render("html/index")
+app.get('/', (req, res) => {
+    res.render("index")
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
