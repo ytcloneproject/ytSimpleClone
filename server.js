d8c19d6b9d4ddc402654e9710aa2ae9c84cad11c
Mitchell Long <mitchelltlong@gmail.com>
2019-07-20 10:58:00 -0700

    Can use create account feature

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const path = require('path')
 const express = require('express')
 const bcrypt = require('bcrypt')
 const MongoClient = require('mongodb').MongoClient
+const mongoose = require('mongoose');
 
 let app = express()
 const port = 3000
@@ -13,22 +14,35 @@ app.get('/', (req, res) => {
 });
 
 app.post('/createAccount', function (req, res) {
-    const username = req.body.username
+    const username = req.body.user
     const email = req.body.email
-    const password = req.body.password
+    let password = req.body.password
 
     const saltRounds = 10;
 
-    bcrypt.hash(password, saltRounds, function(err, hash) {
-        const uri = "mongodb+srv://user1112:ytSimpleClone397@trial1-q1t8x.mongodb.net/test?retryWrites=true&w=majority"
-        const client = new MongoClient(uri, { useNewUrlParser: true })
+    hsh = bcrypt.hash(password, saltRounds);
 
-        client.connect(err => {
+    const uri = "mongodb+srv://user1112:ytSimpleClone397@trial1-q1t8x.mongodb.net/test?retryWrites=true&w=majority";
+    const mongoClient = new MongoClient(uri, { useNewUrlParser: true });
+
+    /*
+    IMPORTANT:
+    Whitelist IP ADDRESS
+    */
+
+    mongoClient.connect(function(err, client) {
+        if (err) {
+            console.error('An error occurred connecting to MongoDB: ', err);
+        } else {
+            var myobj = { name: username, email: email, password: hsh };
             const collection = client.db("ytsc").collection("users")
 
-            var myobj = { name: username, email: email, password: hash }
-            collection.insert(myObj)
-        });
+            collection.insertOne(myobj, function(err, res) {
+                if (err) throw err;
+                console.log("1 document inserted");
+                mongoClient.close();
+            });
+        }
     });
 
     res.end()
@@ -39,7 +53,6 @@ app.post('/login', function (req, res) {
     const password = req.body.password
 
     const saltRounds = 10;
-    const myPlaintextPassword = 's0/\/\P4$$w0rD';
     const someOtherPlaintextPassword = 'not_bacon';
 
     bcrypt.hash(myPlaintextPassword, saltRounds, function(err, hash) {
@@ -48,5 +61,10 @@ app.post('/login', function (req, res) {
 
     res.end()
 });
+
+var upload = function(file) {
+    var cloudinary = require('cloudinary').v2;
+    cloudinary.uploader.upload("my_image.jpg", function(error, result) {console.log(result, error)});
+}
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
