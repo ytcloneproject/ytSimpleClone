c09f688e09d10c56475df35dd6e3a8a5cb2f518c
Mitchell Long <mitchelltlong@gmail.com>
2019-07-19 22:40:18 -0700

    Updated HTML page with login/create account feature

--- server.js
+++ server.js
@@ -1,34 +1,52 @@
 const path = require('path')
 const express = require('express')
-const bcrpy = require('bcrypt')
+const bcrypt = require('bcrypt')
+const MongoClient = require('mongodb').MongoClient
 
 let app = express()
 const port = 3000
 
-app.set('view engine', 'pug')
 app.use(express.urlencoded())
-
-/* MONGO */
-const MongoClient = require('mongodb').MongoClient
-const uri = "mongodb+srv://user1112:ytSimpleClone397@trial1-q1t8x.mongodb.net/test?retryWrites=true&w=majority"
-const client = new MongoClient(uri, { useNewUrlParser: true })
-
-var clientfunc = function() {
-    client.connect(err => {
-        const collection = client.db("ytsc").collection("users")
-        client.close()
-    });
-}
 
 app.get('/', (req, res) => {
      res.sendFile(path.join(__dirname + '/html/index.html'));
 });
 
-app.post('/handler', function (req, res) {
+app.post('/createAccount', function (req, res) {
+    const username = req.body.username
+    const email = req.body.email
+    const password = req.body.password
+
+    const saltRounds = 10;
+
+    bcrypt.hash(password, saltRounds, function(err, hash) {
+        const uri = "mongodb+srv://user1112:ytSimpleClone397@trial1-q1t8x.mongodb.net/test?retryWrites=true&w=majority"
+        const client = new MongoClient(uri, { useNewUrlParser: true })
+
+        client.connect(err => {
+            const collection = client.db("ytsc").collection("users")
+
+            var myobj = { name: username, email: email, password: hash }
+            collection.insertOne(myObj)
+
+            client.close()
+        });
+    });
+
+    res.end()
+});
+
+app.post('/login', function (req, res) {
     const username = req.body.username
     const password = req.body.password
 
+    const saltRounds = 10;
+    const myPlaintextPassword = 's0/\/\P4$$w0rD';
+    const someOtherPlaintextPassword = 'not_bacon';
 
+    bcrypt.hash(myPlaintextPassword, saltRounds, function(err, hash) {
+
+    });
 
     res.end()
 });
